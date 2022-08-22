a15283f72088d124db1644de4f6d0a2d2233be75
Marius <mariushaf@gmail.com>
2022-08-22 08:51:21 +0300

    Init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('app.js');
+const name = 'James'
+
+const fullNmame = `${name} Bond`
+
+console.log('fullNmame ---->', fullNmame);
+
+import { hoursToMin } from "./helper";
+console.log('minutes(12) ---->', hoursToMin(2));
+
+// ctrl + c iseiti is  node
+// node app.js - ijungti node
+// touch - sukurti faila
+// cd switch derectory
